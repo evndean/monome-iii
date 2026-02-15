ad9ce2985341283c914c1b7e351b759658bb8b7e
Evan Dean <evndean@gmail.com>
2026-02-14 19:45:22 -0500

    more comments

--- arc-rhythm-generator.lua
+++ arc-rhythm-generator.lua
@@ -1,3 +1,21 @@
--- arc rhythm generator
--- inspired by the max patch by stretta
--- https://youtu.be/HM0EBvJe1s0
+--[[
+arc rhythm generator
+
+inspired by the max patch by stretta
+https://youtu.be/HM0EBvJe1s0
+
+
+pages (IDEA):
+- control density
+- control speed
+- generate new patterns
+
+TODO:
+- add markers for trigger
+- add density control
+- add speed control
+- set up midi trigger generation
+- add external midi clock
+- add ability to generate new patterns
+- add tests (? does this make sense to do ?)
+]]
