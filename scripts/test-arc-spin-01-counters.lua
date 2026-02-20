a35dd48fb7333e99a1e32f32e1b84b3c5a4a2d4a
Evan Dean <evndean@gmail.com>
2026-02-19 21:28:28 -0500

    add test

---
+++ scripts/test-arc-spin-01-counters.lua
@@ -0,0 +1,56 @@
+--[[
+test arc spin
+
+very basic experiment in rotating leds.
+
+note: because we're using counts + modular arithmetic, we can't ever reverse direction.
+in practice, we probably don't want this; we want to be able to reverse direction.
+
+]]
+
+local pos = { 1, 1, 1, 1 }
+local speed = { 1, 1, 1, 1 } -- num ticks needed before advancing
+local count = { 0, 0, 0, 0 }
+local responsiveness = { 1, 10, 100, 500 }
+local brightness = 15
+local refresh_in_ms = 12
+local refresh = false
+
+function arc(ring, delta)
+    speed[ring] = clamp(speed[ring] - delta, 1, 64)
+end
+
+local function step_advance()
+    for ring = 1, 4 do
+        count[ring] = count[ring] + 1
+        if count[ring] % speed[ring] == 0 then
+            pos[ring] = wrap(pos[ring] + 1, 1, 64)
+            refresh = true
+        end
+    end
+end
+
+local function redraw()
+    if refresh then
+        for ring = 1, 4 do
+            arc_led_all(ring, 0)
+            arc_led(ring, pos[ring], brightness)
+        end
+        arc_refresh()
+        refresh = false
+    end
+end
+
+
+local function setup()
+    for ring = 1, 4 do
+        arc_led_all(ring, 0)
+        arc_res(ring, responsiveness[ring])
+    end
+    arc_refresh()
+end
+
+setup()
+
+metro.new(step_advance, refresh_in_ms)
+metro.new(redraw, refresh_in_ms)
