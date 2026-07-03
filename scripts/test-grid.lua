490c6bccab8d6092c9c535b866aff1b3923906c3
Evan Dean <evndean@gmail.com>
2026-07-03 19:05:43 -0400

    update test-grid

--- scripts/test-grid.lua
+++ scripts/test-grid.lua
@@ -1,7 +1,7 @@
 grid_led_all(0)
 grid_refresh()
 
-function grid(x,y,z) -- callback for grid keypresses. example to print key data:
+function event_grid(x,y,z) -- callback for grid keypresses. example to print key data:
     ps("grid %d %d %d",x,y,z)
     grid_led(x,y,z*3+1)
     grid_refresh()
