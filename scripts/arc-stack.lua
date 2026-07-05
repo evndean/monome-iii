37cbcb2e506c178bbd5d36f7ffc76a4162c79b67
Evan Dean <evndean@gmail.com>
2026-07-05 00:24:03 -0400

    stub out redraw

--- scripts/arc-stack.lua
+++ scripts/arc-stack.lua
@@ -13,7 +13,8 @@ not sure whether that should be the first or the last tho.
 knob for individual segments will control speed.
 knob for combination could control... number of note trigger points maybe?
 ]]
---
+
+local refresh_rate = 0.012 -- 12 ms
 
 -- ## ring ##
 --
@@ -71,3 +72,69 @@ knob for combination could control... number of note trigger points maybe?
 -- 2. update the state of the combo ring (i.e. add up values across the other three rings).
 -- 3. update the state at each note based on the state of the combo ring.
 -- 4. if the value has changed, send a new note (also need to end the old one).
+
+---Log formatted string with timestamp.
+---@param formatted_string string
+---@param ... any
+local function log(formatted_string, ...)
+	s = "[%f] " .. formatted_string
+	ps(s, get_time(), ...)
+end
+
+local function set_ring_led(self, i, v)
+	self.leds[i] = v
+end
+
+-- TODO: is there a better way to do this?
+local function get_ring_leds(self)
+	return self.leds
+end
+
+-- initialize ring 1
+local r1 = {
+	leds = {},
+	set_led = set_ring_led,
+	get_leds = get_ring_leds,
+}
+for i = 1, 64 do
+	r1:set_led(i, i > 32 and 0 or 15)
+end
+
+local function redraw()
+	log("redraw")
+
+	-- get LEDs for rings 1-3
+	local r1_leds = r1:get_leds()
+	log("r1_leds: %s", table.concat(r1_leds, ","))
+
+	for i = 1, 64 do
+		-- draw rings 1-3
+		arc_led(1, i, r1_leds[i])
+
+		-- combine rings 1-3 to determine ring 4
+		local combined = r1_leds[i] > 0 and 2 or 0
+		arc_led(4, i, combined)
+	end
+
+	arc_refresh()
+end
+
+local function setup()
+	log("setup")
+
+	-- reset arc sensitivity
+	for ring = 1, 4 do
+		arc_res(ring, 1)
+	end
+
+	-- zero out arc LEDs
+	arc_led_all(0)
+
+	-- redraw arcs (if we do this, can probably skip the "zero out" step above)
+	redraw()
+end
+
+setup()
+
+-- local m = metro.init(redraw, refresh_rate)
+-- m:start()
